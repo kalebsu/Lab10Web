77ff84ea5c2ffdb8fd940b8cad194ad2234304d6
kalebsu <92831647+kalebsu@users.noreply.github.com>
2022-12-11 09:43:22 +0700

    Menambahkan file mobil.php

---
+++ mobil.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Program sederhana pendefinisian class dn pemanggilan class
+ **/
+
+ class Mobil
+ {
+    private $warna;
+    private $merk;
+    private $harga;
+
+    public function __construct()
+    {
+        $this->warna = "Biru";
+        $this->merk = "BMW";
+        $this->harga = "10000000";
+    }
+
+    public function gantiWarna($warnaBaru){
+        $this->warna = $warnaBaru;
+    }
+
+    public function tampilWarna(){
+        echo "Warna mobilnya : " . $this->warna;
+    }
+ }
+
+ // Membuat objek mobil
+ $a = new Mobil();
+ $b = new Mobil();
+
+ // Memanggil objek
+ echo "<b>Mobil pertama </b><br>";
+ $a->tampilWarna();
+ echo"<br>Mobil pertama ganti warna : <br>";
+ $a->gantiWarna("Merah");
+ $a->tampilWarna();
+
+ // Memanggil objek
+ echo "<br><b>Mobil keduaa</b><br>";
+ $b->gantiWarna("Hijau");
+ $b->tampilWarna();
+
+ ?>
